89782f70f96195538a83c464617293f5a37e0d1e
--get <debadarshanmaharana248@gmail.com>
2025-01-12 15:24:32 +0530

    cartContext used in external

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['src/context/cartContext'],
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
